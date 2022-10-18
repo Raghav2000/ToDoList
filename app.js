202df91525b8d9f8900bbea99ed2775bffc4272a
Raghav2000 <raghav292000@gmail.com>
2022-10-18 22:37:48 +0530

    Save files

---
+++ app.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.set('view engine', 'ejs');
+app.get("/", function(req,res){
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US",options);
+
+  res.render("list", {kindOfDay:day, newListItems:items});
+});
+
+app.post("/",function(req,res){
+  var item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/")
+});
+
+app.listen(3000,function(){
+  console.log("Server is started at port 3000");
+});
